168d89e7c26838d69726850f02a0a11dff66b731
202610338 김대건 <daegun10338@kbu.ac.kr>
2026-09-18 14:15:47 +0900

    프로그래밍언어할용 3주차

---
+++ sample03-1/src/Main.java
@@ -0,0 +1,11 @@
+void main() {
+    boolean flag;     //false;
+    byte value1 = 127;         // -128 ~ 127
+    short value2 = 32767;         // - 32, 768 ~ + 32, 767
+    int value3 = 444444444;
+    long value4 = 444444444444444444l;
+    double value5 = 4.5555555555555555555;
+    float value6 = 4.555555555555555555555555555f;
+    char value7 = 'A';
+    String value8 = "AB";
+}
